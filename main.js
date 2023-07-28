72964b5b503f9efc1e9536af5fb570ad59178088
YunJeong Ha <gkdbswlwjd11@naver.com>
2023-07-28 21:56:56 +0900

    second commit

--- main.js
+++ main.js
@@ -1,73 +1,79 @@
-var clock = document.querySelector(".clock");
+let clock = document.querySelector(".clock");
+const batteryElement = document.querySelector(".battery");
+const onecolor = document.querySelector('.one');
+const twocolor = document.querySelector('.two');
+const threecolor = document.querySelector('.three');
+const fourcolor = document.querySelector('.four');
+const fivecolor = document.querySelector('.five');
 
-function getTime() {
-    const time = new Date();
-    const hour = time.getHours();
-    const minutes = time.getMinutes();
-    const seconds = time.getSeconds();
-    clock.innerHTML = `${hour<10 ? `0${hour}`:hour} : ${minutes<10 ? `0${minutes}`:minutes} : ${seconds<10 ? `0${seconds}`:seconds}`
-}
+let alarmhour = document.getElementById("hour-input");
+let alarmminute = document.getElementById("minute-input");
+let alarmsecond = document.getElementById("second-input");
+let addAlarmBtn = document.getElementById("add-alarm-btn");
+let alarmsList = document.getElementById("alarms-list");
+let count = 0;
 
-var onecolor = document.querySelector('.one');  //red
-var twocolor = document.querySelector('.two');  //blue
-var threecolor = document.querySelector('.three');
-var fourcolor = document.querySelector('.four');
-var fivecolor = document.querySelector('.five');
+var Alarm = {
+    batteryLevel: 100,
 
-var batteryElement = document.querySelector(".battery");
-var batteryLevel = 100;
-var timecolor = document.querySelector('.time');
+    //현재시간을 출력하는 메서드
+    getTime: function() {
+        const time = new Date();
+        const hour = time.getHours();
+        const minutes = time.getMinutes();
+        const seconds = time.getSeconds();
+        clock.innerHTML = `${hour<10 ? `0${hour}`:hour} : ${minutes<10 ? `0${minutes}`:minutes} : ${seconds<10 ? `0${seconds}`:seconds}`
+    },
 
-function updateBattery() {
-    batteryElement.innerHTML = `배터리 : ${batteryLevel}%`;
-    batteryLevel -= 1;
-    if (batteryLevel == -1) {
-        onecolor.style.backgroundColor = "bisque";
-        timecolor.style.backgroundColor = "black";
-        clearInterval(batteryInterval);
-        alert("배터리가 모두 소진되었습니다.")
+    //배터리 잔량을 표시하는 메서드
+    updateBattery: function() {
+        batteryElement.innerHTML = `배터리 : ${this.batteryLevel}%`;
+        this.batteryLevel -= 1;
+        if (this.batteryLevel == -1) {
+            onecolor.style.backgroundColor = "bisque";
+            timecolor.style.backgroundColor = "black";
+            clearInterval(batteryInterval);
+            alert("배터리가 모두 소진되었습니다.")
+        }
+        if (this.batteryLevel == 79) {
+            fivecolor.style.backgroundColor = "bisque";
+        }
+        if (this.batteryLevel == 59) {
+            fourcolor.style.backgroundColor = "bisque";
+        }
+        if (this.batteryLevel == 39) {
+            threecolor.style.backgroundColor = "bisque";
+        }
+        if (this.batteryLevel == 19) {
+            twocolor.style.backgroundColor = "bisque";
+        }
+    },
+
+    //알람을 추가하는 메서드
+    addAlarm: function() {
+        if (count >= 3) {
+            alert("알림은 3개까지만 추가 가능합니다.");
+            return;
+        }
+        const alarmhours = alarmhour.value;
+        const alarmminutes = alarmminute.value;
+        const alarmseconds = alarmsecond.value;
+
+        if (alarmhours && alarmminutes && alarmseconds) {
+            const listItem = document.createElement("li");
+            listItem.innerHTML = `${alarmhours<10 ? `0${alarmhours}`:alarmhours} : ${alarmminutes<10 ? `0${alarmminutes}`:alarmminutes} : ${alarmseconds<10 ? `0${alarmseconds}`:alarmseconds}`;
+            alarmsList.appendChild(listItem);
+            count += 1;
+        }
+    },
+
+    init: function() {
+        setInterval(this.getTime.bind(this), 1000);
+        setInterval(this.updateBattery.bind(this), 1000);
+        addAlarmBtn.addEventListener("click", this.addAlarm.bind(this));
     }
-    if (batteryLevel == 79) {
-        fivecolor.style.backgroundColor = "bisque";
-    }
-    if (batteryLevel == 59) {
-        fourcolor.style.backgroundColor = "bisque";
-    }
-    if (batteryLevel == 39) {
-        threecolor.style.backgroundColor = "bisque";
-    }
-    if (batteryLevel == 19) {
-        twocolor.style.backgroundColor = "bisque";
-    }
-}
+};
 
-var alarmhour = document.getElementById("hour-input");
-var alarmminute = document.getElementById("minute-input");
-var alarmsecond = document.getElementById("second-input");
-var addAlarmBtn = document.getElementById("add-alarm-btn");
-var alarmsList = document.getElementById("alarms-list");
-var count = 0;
-function addAlarm() {
-    if (count >= 3) {
-        alert("알림은 3개까지만 추가 가능합니다.");
-        return;
-    }
-    const alarmhours = alarmhour.value;
-    const alarmminutes = alarmminute.value;
-    const alarmseconds = alarmsecond.value;
+Alarm.init();
 
-    if (alarmhours && alarmminutes && alarmseconds) {
-        const listItem = document.createElement("li");
-        listItem.innerHTML = `${alarmhours<10 ? `0${alarmhours}`:alarmhours} : ${alarmminutes<10 ? `0${alarmminutes}`:alarmminutes} : ${alarmseconds<10 ? `0${alarmseconds}`:alarmseconds}`;
-        alarmsList.appendChild(listItem);
-        count += 1;
-    }
-}
 
-function init(){
-    setInterval(getTime, 1000);
-    batteryInterval = setInterval(updateBattery, 1000);
-    addAlarmBtn.addEventListener("click", addAlarm);
-}
-
-init();
